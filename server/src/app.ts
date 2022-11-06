8942accaf0e2ed1e170a85ca7bbb992d5e482587
Erick <rvoltpwn@gmail.com>
2022-11-06 00:05:20 -0600

    Test

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,7 +6,7 @@ import xss from 'xss-clean';
 import compression from 'compression';
 
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import path, { dirname } from 'path';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
@@ -34,9 +34,9 @@ app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(compression());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(`${__dirname}/../../client/dist`));
+  app.use(express.static(path.join(__dirname, '../../client/dist')));
   app.get('*', function (_request, response) {
-    response.sendFile(`${__dirname}/../../client/dist/index.html`);
+    response.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   });
 }
 
