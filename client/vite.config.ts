33dd59e35bf59d5a7005a8bf91dedd533206cc6b
Erick <rvoltpwn@gmail.com>
2022-11-06 19:18:41 -0600

    Fixes Assests images cleanup Added CSP

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import svgr from 'vite-plugin-svgr';
-import csp from 'vite-plugin-csp';
-
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    csp(),
     svgr(),
     nodePolyfills({
       protocolImports: true,
